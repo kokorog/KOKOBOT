cd91198cd3da7dc0783769f6ecbd97e1150511c5
kokorog <47898925+kokorog@users.noreply.github.com>
2019-02-22 21:45:40 +0200

    Update kokobot.js

--- kokobot.js
+++ kokobot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+BOT.TOKEN = NTM5NzY0NTYzNDA3NjY3MjEw.D1HaKA.xVSp17P-I5JeiVmQUA9bKGlU86o
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
